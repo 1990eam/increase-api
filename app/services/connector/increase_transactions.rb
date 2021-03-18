2717ddb9385dc025174cfc0d66acfd241ec73ec5
Elian Magni <magni.elian@gmail.com>
2021-03-18 14:35:14 -0300

    Added max_retries to connector

--- app/services/connector/increase_transactions.rb
+++ app/services/connector/increase_transactions.rb
@@ -10,6 +10,20 @@ class Connector::IncreaseTransactions
 
     https = Net::HTTP.new(url.host, url.port)
     https.use_ssl = true
+    https.max_retries = 5
+
+    request = Net::HTTP::Get.new(url)
+    request["Authorization"] = "Bearer 1234567890qwertyuiopasdfghjklzxcvbnm"
+
+    response = https.request(request)
+  end
+
+  def fetch_client(client_identification)
+    url = URI("#{INCREASE_BASE_URL}/clients/#{client_identification}")
+
+    https = Net::HTTP.new(url.host, url.port)
+    https.use_ssl = true
+    https.max_retries = 5
 
     request = Net::HTTP::Get.new(url)
     request["Authorization"] = "Bearer 1234567890qwertyuiopasdfghjklzxcvbnm"
