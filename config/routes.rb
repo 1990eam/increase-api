0c2f1dd1bac49c46e50b1f55b19884a066b1724c
Elian Magni <magni.elian@gmail.com>
2021-03-20 13:14:58 -0300

    Removed payments endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :payments, only: [ :index ]
       resources :clients, only: [ :index ]
 
       resources :clients, only: [:show] do
